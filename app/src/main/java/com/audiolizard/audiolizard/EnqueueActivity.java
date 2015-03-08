99de583e4cfabbb30a41f69ccbd4a337e65d99b1
Yang Zhang <yaaang@gmail.com>
2015-03-07 19:46:02 -0800

    Fix error dialog logic

--- app/src/main/java/com/audiolizard/audiolizard/EnqueueActivity.java
+++ app/src/main/java/com/audiolizard/audiolizard/EnqueueActivity.java
@@ -14,7 +14,6 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URLEncodedUtils;
@@ -31,48 +30,54 @@ public class EnqueueActivity extends ActionBarActivity {
 
     public String TAG = "aoeu";
 
-    class RequestTask extends AsyncTask<String, String, String> {
+    class Response {
+        public String response;
+        public Exception error;
+    }
+
+    class RequestTask extends AsyncTask<String, String, Response> {
 
         @Override
-        protected String doInBackground(String... uri) {
+        protected Response doInBackground(String... uri) {
             HttpClient httpclient = new DefaultHttpClient();
             HttpResponse response;
-            String responseString;
+            Response resp = new Response();
             try {
                 response = httpclient.execute(new HttpGet(uri[0]));
                 StatusLine statusLine = response.getStatusLine();
                 if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
                     ByteArrayOutputStream out = new ByteArrayOutputStream();
                     response.getEntity().writeTo(out);
-                    responseString = out.toString();
+                    resp.response = out.toString();
                     out.close();
                 } else {
                     //Closes the connection.
                     response.getEntity().getContent().close();
                     throw new IOException(statusLine.getReasonPhrase());
                 }
-            } catch (ClientProtocolException e) {
-                Log.e(TAG, "exception", e);
-                responseString = e.toString();
             } catch (IOException e) {
                 Log.e(TAG, "exception", e);
-                responseString = e.toString();
+                resp.error = e;
             }
-            return responseString;
+            return resp;
         }
 
         @Override
-        protected void onPostExecute(String result) {
+        protected void onPostExecute(Response result) {
             super.onPostExecute(result);
-            AlertDialog.Builder builder = new AlertDialog.Builder(EnqueueActivity.this);
-            builder.setMessage(result)
-                    .setNeutralButton("OK", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            finish();
-                        }
-                    });
-            // Create the AlertDialog object and return it
-            builder.create().show();
+            if (result.error != null) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(EnqueueActivity.this);
+                builder.setMessage(result.error.toString())
+                        .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                finish();
+                            }
+                        });
+                // Create the AlertDialog object and return it
+                builder.create().show();
+            } else {
+                finish();
+            }
         }
     }
 
